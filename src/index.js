1d3eaa97d9a7556af0ca4969ec705a5fbfc532ae
Burkun <electronik_est@mail.ru>
2020-10-22 17:09:06 +0300

    feat: add solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('');
+  let bracket = [];
+  if (str.length % 2 != 0) return false;
+  let i = 0;
+  while (i < str.length) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][1] && bracket[bracket.length - 1] == bracketsConfig[j][0]) bracket.pop();
+      else if (str[i] == bracketsConfig[j][0]) bracket.push(str[i])
+    }
+    i++
+  }
+  if (bracket.length == 0) return true;
+  else return false;
 }
